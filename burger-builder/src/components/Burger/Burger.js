d89f19795190c1743e511479ff09e2c28c967af2
karunya <tantatikarunya@gmail.com>
2021-06-22 12:22:50 +0530

    basic modal

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -2,13 +2,24 @@ import React from "react";
 import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = (props) => {
+  let transformedingredients=Object.keys(props.ingredients)
+  .map(igKey=>{
+    return [...Array(props.ingredients[igKey])].map((_,i)=>{
+    return <BurgerIngredient ke={igKey +1} type={igKey}/>;
+  });
+  }).reduce((arr,el)=>{
+    return arr.concat(el)
+  },[]);
+  if(transformedingredients.length===0){
+transformedingredients=<p>Please start adding ingredients!</p>
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedingredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
 };
 export default burger;
+;
